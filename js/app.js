0b12588e80252f58bce4cb7ec943efd98c8de5b5
LaraR <laragoblin@gmail.com>
2020-01-19 12:27:51 +0100

    kejarhfkwfj

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function getData()
+{
+    var nombre=document.getElementById("email_inline").value;
+    //var edad=document.getElementById("edad").value;
+    alert("Tu nombre es : "+email_inline+);  
+}
